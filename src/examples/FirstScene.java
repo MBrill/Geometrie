f44f92e9305a7dcf55e94c74db308ab4a92ebf77
Manfred Brill <manfred.brill@hs-kl.de>
2018-08-17 16:35:17 +0200

    Ausgangszustand

--- src/examples/FirstScene.java
+++ src/examples/FirstScene.java
@@ -1,19 +1,24 @@
 package examples;
 
+import org.apache.log4j.Logger;
+
+
 import geometry.*;
 import geometry.shapes.*;
 
 public class FirstScene {
 
-
+	private static Logger log = LogManager.getLogger("FirstScene");
 	
 	public static void main(String[] args) {
+		
+		log.trace(">> FirstScene.Main");
+		
 		Scene scene = new Scene();
 		Line line = new Line();
 		Rectangle box = new Rectangle();
 		Triangle triangle = new Triangle();
 		Circle circle = new Circle();
-		
 		
 		scene.addObject(line);
 		scene.addObject(box);
@@ -22,6 +27,8 @@ public class FirstScene {
 		
 		System.out.println("Die Szene");
 		System.out.println(scene);
+		
+		log.trace("<< FirstScene.Main");
 	}
 
 }
