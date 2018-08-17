373df4f1198a570fa605ac76a889f2394c4f3630
Manfred Brill <manfred.brill@hs-kl.de>
2018-08-17 16:37:19 +0200

    Jetzt wirklich wieder Ausgangszustand

--- src/examples/FirstScene.java
+++ src/examples/FirstScene.java
@@ -1,19 +1,11 @@
 package examples;
 
-import org.apache.log4j.Logger;
-
-
 import geometry.*;
 import geometry.shapes.*;
 
 public class FirstScene {
 
-	private static Logger log = LogManager.getLogger("FirstScene");
-	
 	public static void main(String[] args) {
-		
-		log.trace(">> FirstScene.Main");
-		
 		Scene scene = new Scene();
 		Line line = new Line();
 		Rectangle box = new Rectangle();
@@ -27,8 +19,6 @@ public class FirstScene {
 		
 		System.out.println("Die Szene");
 		System.out.println(scene);
-		
-		log.trace("<< FirstScene.Main");
-	}
+			}
 
 }
