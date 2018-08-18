cc58189ddae9096c6dc0345ab86d97393b60db04
Manfred Brill <manfred.brill@hs-kl.de>
2018-08-18 18:12:58 +0200

    Quellen zum Text, ohne die Lösung der Aufgabe
    Die Datei Logs.txt ignorieren wir, sie ist die Ausgabe
    mit Hilfe des Filehandlers.

--- src/examples/FirstScene.java
+++ src/examples/FirstScene.java
@@ -1,4 +1,13 @@
+/*
+ * Wir könnten verschiedene Versionen dieser Datei erzeugen, da im Text zu diesem
+ * Beispiel auch verschiedene Varianten beschrieben werden.
+ * 
+ * Aktuell ist dies durch Kommentar-Zeichen gelöst.
+ */
 package examples;
+
+import java.io.IOException;
+import java.util.logging.*;
 
 import geometry.*;
 import geometry.shapes.*;
@@ -6,6 +15,30 @@ import geometry.shapes.*;
 public class FirstScene {
 
 	public static void main(String[] args) {
+		
+		final Logger log = Logger.getLogger(FirstScene.class.getName());
+		//final Logger log = Logger.getLogger("");
+		
+		final Handler systemOut = new ConsoleHandler();
+		systemOut.setLevel(Level.CONFIG);
+		log.addHandler( systemOut);
+		log.setLevel(Level.CONFIG);	
+		log.setUseParentHandlers(false);
+		
+		/*final FileHandler fileTxt;
+		final SimpleFormatter formatter = new SimpleFormatter();
+		
+		try {
+			fileTxt = new FileHandler("Logs.txt");
+			fileTxt.setFormatter(formatter);
+			fileTxt.setLevel(Level.ALL);
+			log.addHandler(fileTxt);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}*/
+
+		log.info(">> FirstScene.Main");
+		
 		Scene scene = new Scene();
 		Line line = new Line();
 		Rectangle box = new Rectangle();
@@ -19,6 +52,7 @@ public class FirstScene {
 		
 		System.out.println("Die Szene");
 		System.out.println(scene);
-			}
 
+		log.info(">> FirstScene.Main");
+	}
 }
--- src/geometry/shapes/Line.java
+++ src/geometry/shapes/Line.java
@@ -1,21 +1,32 @@
 package geometry.shapes;
 
+import java.util.logging.*;
+
 import geometry.Point2D;
 import geometry.Shape;
 
 public class Line extends Shape {
 
+	private Logger lineLog = Logger.getLogger("");
+	
 	Point2D begin = new Point2D(0.0, 0.0);
 	Point2D end = new Point2D(1.0, 0.0);
 	
 	public Line() {
+		   lineLog.info(">> Line()");
            this.refPoint = this.computeRefPoint();
+           lineLog.info("** Linie mit Referenzpunkt" + refPoint);
+           lineLog.info("<< Line()");
 	}
 	
 	public Line(Point2D begin, Point2D end) {
+		   lineLog.info(">> Line(Point2D, Point2D)");
 		this.begin = begin;
 		this.end = end;
         this.refPoint = this.computeRefPoint();
+        
+        lineLog.info("** Linie mit Referenzpunkt" + refPoint);        
+        lineLog.info("<< Line(Point2D, Point2D)");
 	}
 	
 	   
