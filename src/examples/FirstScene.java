b80b2d1f22c70d312149b5c06f915150dad198ce
Manfred Brill <manfred.brill@hs-kl.de>
2018-08-20 10:36:43 +0200

    Lösun g für logback und slf4j
    Funktioniert alles, aber der FileAppender zickt noch. Das könnte
    an den Pfaden und Schreibrechten liegen. Muss noch gefixt werden.
    
    In Rectangle.java wurde nur die Umlaute korrigiert, das ist nicht
    die Lösung einer Aufgabe!

--- src/examples/FirstScene.java
+++ src/examples/FirstScene.java
@@ -1,11 +1,18 @@
 package examples;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import geometry.*;
 import geometry.shapes.*;
 
 public class FirstScene {
 
 	public static void main(String[] args) {
+		
+		final Logger log = LoggerFactory.getLogger(FirstScene.class.getName());
+		log.info(">> FirstScene.main");
+		
 		Scene scene = new Scene();
 		Line line = new Line();
 		Rectangle box = new Rectangle();
@@ -19,6 +26,8 @@ public class FirstScene {
 		
 		System.out.println("Die Szene");
 		System.out.println(scene);
-			}
+		
+		log.info("<< FirstScene.main");
+	}
 
 }
--- src/geometry/shapes/Line.java
+++ src/geometry/shapes/Line.java
@@ -1,21 +1,33 @@
 package geometry.shapes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import examples.FirstScene;
 import geometry.Point2D;
 import geometry.Shape;
 
 public class Line extends Shape {
+	
+	final Logger lineLog = LoggerFactory.getLogger(Line.class.getName());
 
 	Point2D begin = new Point2D(0.0, 0.0);
 	Point2D end = new Point2D(1.0, 0.0);
 	
 	public Line() {
+	       lineLog.trace(">> Line()");
            this.refPoint = this.computeRefPoint();
+           lineLog.info("** Linie mit Referenzpunkt {}", refPoint);
+           lineLog.trace("<< Line()");
 	}
 	
 	public Line(Point2D begin, Point2D end) {
+	    lineLog.trace(">> Line(Point2D, Point2D)");
 		this.begin = begin;
 		this.end = end;
         this.refPoint = this.computeRefPoint();
+        lineLog.info("** Linie mit Referenzpunkt {}", refPoint);
+	    lineLog.trace("<< Line(Point2D, Point2D)");
 	}
 	
 	   
