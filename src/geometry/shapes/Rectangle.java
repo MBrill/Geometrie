cd8d6f9b8bd2cc12f3590fa1e9c958cacfc9498e
MBrill <manfred.brill@hs-kl.de>
2019-07-30 09:33:24 +0200

    Musterlösung der Aufgabe 3.2 in Logging
    Der Klasse Rectangle werden analog zur Klasse Line
    Logging-Ausgaben hinzugefügt.

--- src/geometry/shapes/Rectangle.java
+++ src/geometry/shapes/Rectangle.java
@@ -3,8 +3,13 @@ package geometry.shapes;
 import geometry.Point2D;
 import geometry.Shape;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Rectangle extends Shape {
 
+    private static final Logger rectLog = LogManager.getLogger();
+    
 	private Point2D base = new Point2D(0.0, 0.0);
 	
 	private double width = 1.0;
@@ -12,21 +17,30 @@ public class Rectangle extends Shape {
 	private double height = 1.0;
 	
 	public Rectangle() {
-            this.refPoint = this.computeRefPoint();
+		rectLog.trace(">> Rectangle()");
+        this.refPoint = this.computeRefPoint();
+        rectLog.info("** Rechteck mit Referenzpunkt {}", refPoint);
+        rectLog.trace("<< Rectangle()");
 	}
 	
 	public Rectangle(Point2D base, double width, double height) {
+		rectLog.trace(">> Rectangle(Point2D, double, double)");
 		this.base = base;
 		this.width = width;
 		this.height = height;
         this.refPoint = this.computeRefPoint();
+        rectLog.info("** Rechteck mit Referenzpunkt {}", refPoint);
+		rectLog.trace("<< Rectangle(Point2D, double, double)");
 	}
 	
 	public Rectangle(Point2D base, double dimension) {
+		rectLog.trace(">> Rectangle(Point2D, double)");
 		this.base = base;
 		this.width = dimension;
 		this.height = dimension;
         this.refPoint = this.computeRefPoint();
+        rectLog.info("** Rechteck mit Referenzpunkt {}", refPoint);
+		rectLog.trace("<< Rectangle(Point2D, double)");
 	}
 	
 	@Override
