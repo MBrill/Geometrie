1393605a75f8ae3ba68ee53e30e3ab4f491cd62d
MBrill <manfred.brill@hs-kl.de>
2021-09-03 08:59:27 +0200

    Javadoc für pages

---
+++ docs/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"geometry.shapes","l":"Circle"},{"p":"examples","l":"FirstScene"},{"p":"geometry.shapes","l":"Line"},{"p":"geometry","l":"Point2D"},{"p":"geometry.shapes","l":"Rectangle"},{"p":"geometry","l":"Scene"},{"p":"geometry","l":"Shape"},{"p":"geometry.shapes","l":"Triangle"}]
